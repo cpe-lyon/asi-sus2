ad306a173f711249897d865eaabbf0512a3b5aad
B4tiste <batiste.laloi@gmail.com>
2023-05-24 14:47:48 +0200

    Formulaire ajout Poney + Affichage liste Poneys

--- src/main/java/com/sus_society/sus2/controller/RequestCrt.java
+++ src/main/java/com/sus_society/sus2/controller/RequestCrt.java
@@ -1,9 +1,11 @@
 package com.sus_society.sus2.controller;
 
 import com.sus_society.model.Poney;
+import com.sus_society.model.PoneyFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -37,6 +39,26 @@ public class RequestCrt {
     public String ukView(Model model) {
         model.addAttribute("myPoney", poneyDao.getRandomPoney());
         return "uk/poneyView";
+    }
+
+    @RequestMapping(value = { "/uk/addPoney" }, method = RequestMethod.GET)
+    public String ukAddPoney(Model model) {
+        PoneyFormDTO poneyForm = new PoneyFormDTO();
+        model.addAttribute("poneyForm", poneyForm);
+        return "uk/poneyForm";
+    }
+
+    @RequestMapping(value = { "/uk/addPoney" }, method = RequestMethod.POST)
+    public String ukAddPoney(Model model, @ModelAttribute("poneyForm") PoneyFormDTO poneyForm) {
+        Poney p = poneyDao.addPoney(poneyForm.getName(), poneyForm.getColor(), poneyForm.getSuperPower(), poneyForm.getImgUrl());
+        model.addAttribute("myPoney", p);
+        return "uk/poneyView";
+    }
+
+    @RequestMapping(value = { "/uk/list" }, method = RequestMethod.GET)
+    public String ukViewList(Model model) {
+        model.addAttribute("poneyList", poneyDao.getPoneyList());
+        return "uk/poneyViewList";
     }
 
 }
