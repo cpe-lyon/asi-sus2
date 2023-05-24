673e1669bc9c43ff4bfcd7bab5468a972a981964
Johan <johan.planchon@cpe.fr>
2023-05-24 08:30:34 +0000

    feat: added project base

---
+++ src/main/java/com/sus_society/sus2/Sus2Application.java
@@ -0,0 +1,13 @@
+package com.sus_society.sus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sus2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sus2Application.class, args);
+    }
+
+}
